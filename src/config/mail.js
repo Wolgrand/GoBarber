ca23404b5eb3cd6d880c6214d83ab90a15fe951b
Wolgrand Pinto Lordao Neto <wolgrand.lordao@energisa.com.br>
2019-09-09 08:15:27 -0300

    Add mail feature and Queue Processing

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '2b2b0fd68524e9',
+    pass: '2d022f68793dbc',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
