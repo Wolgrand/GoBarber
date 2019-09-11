3b4adac078438c4553043358f9987d4c1a6ae92f
Wolgrand Pinto Lordao Neto <wolgrand.lordao@energisa.com.br>
2019-09-11 08:18:24 -0300

    ADD Environment Variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '2b2b0fd68524e9',
-    pass: '2d022f68793dbc',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
